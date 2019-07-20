93f70ecd82f20ad478052b3955d959001505da35
Luke <lukerbab@gmail.com>
2019-07-21 00:01:30 +0100

    juuyijh8mn9

--- src/components/LandingPage/Particle/ParticleContainer.js
+++ src/components/LandingPage/Particle/ParticleContainer.js
@@ -4,30 +4,37 @@ import * as particleService from '../../../services/particle.service';
 import {ParticleContainerContext} from './ParticleContainerContext';
 import {WorkContainerContext} from '../Work/WorkContainerContext';
 
-export default function ParticleContainer(props) {
-    const {particleEngine} = useContext(ParticleContainerContext);
-    const {dimensions} = useContext(WorkContainerContext);
+export default React.forwardRef((props, ref) => {
+        const {particleEngine} = useContext(ParticleContainerContext);
+        const {dimensions} = useContext(WorkContainerContext);
 
-    useEffect(() => {
-        const elapsed = Date.now();
-        const containerIdentifier = `particle-container-${props.identifier}`;
-        let canvasContainer = document.getElementById(containerIdentifier);
+        useEffect(() => {
+            if (ref.current) {
+                const {width, height} = ref.current.getBoundingClientRect();
+                particleEngine.renderer.view.width = width;
+                particleEngine.renderer.view.height = height;
+            }
 
-        canvasContainer.appendChild(particleEngine.renderer.view);
-        const destroyParticleEngine = particleService.initiateParticleEngine(particleEngine, elapsed);
+            debugger;
+            const elapsed = Date.now();
+            const containerIdentifier = `particle-container-${props.identifier}`;
+            let canvasContainer = document.getElementById(containerIdentifier);
+            canvasContainer.appendChild(particleEngine.renderer.view);
+            const destroyParticleEngine = particleService.initiateParticleEngine(particleEngine, elapsed);
 
-        return () => {
-            destroyParticleEngine();
-            canvasContainer = null;
-        }
-    }, [
-        props.identifier,
-        particleEngine,
-        dimensions
-    ]);
+            return () => {
+                destroyParticleEngine();
+                canvasContainer = null;
+            }
+        }, [
+            props.identifier,
+            particleEngine,
+            dimensions
+        ]);
 
-    return (
-        <ParticleContainerStyled id={`particle-container-${props.identifier}`} className="particle-container"/>
-    );
-};
+        return (
+            <ParticleContainerStyled id={`particle-container-${props.identifier}`} className="particle-container"/>
+        );
+    }
+);
 
--- src/components/LandingPage/Work/WorkContainer.js
+++ src/components/LandingPage/Work/WorkContainer.js
@@ -1,16 +1,43 @@
 import React from 'react';
 
-import WorkContainerLarge from './WorkContainerLarge';
-import WorkContainerSmall from './WorkContainerSmall';
+import {
+    WorkContainerStyled,
+    WorkDescriptionStyled,
+    WorkHeadingStyled, WorkImageBackgroundStyled, WorkOverlayStyled,
+    WorkSkillsContainerListStyled
+} from './Work.style';
 
-export default function WorkContainer(props) {
-    return (
-        <>
-            {
-                props.workContainerSize === 'large' ?
-                    <WorkContainerLarge workData={props.workData}/> :
-                    <WorkContainerSmall workData={props.workData}/>
-            }
-        </>
-    )
-}
+import ParticleContainer from '../Particle/ParticleContainer';
+
+export default React.forwardRef((props, ref) => {
+        const {title, id, description} = props.workData;
+
+        return (
+            <WorkContainerStyled className="tile is-child notification"
+                                 containerSize="large">
+                <WorkHeadingStyled className="title">
+                    {title}
+                </WorkHeadingStyled>
+
+                <WorkDescriptionStyled>
+                    <p>
+                        {description}
+                    </p>
+                </WorkDescriptionStyled>
+
+                <WorkSkillsContainerListStyled>
+                    <span><b>React</b></span>
+                    <span><b>/ Node</b></span>
+                    <span><b>/ Netlify</b></span>
+                </WorkSkillsContainerListStyled>
+
+                <ParticleContainer ref={ref} identifier={id}/>
+
+                <WorkOverlayStyled>
+                    <WorkImageBackgroundStyled backgroundImage='https://i.ytimg.com/vi/gUIJ-UkQsXI/maxresdefault.jpg'/>
+                </WorkOverlayStyled>
+
+            </WorkContainerStyled>
+        )
+    }
+)
